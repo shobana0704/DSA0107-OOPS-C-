e7cd8b2846468b07e10b76fc3f4b2d4da4287204
shobana0704 <shobzz0704@gmail.com>
2026-07-09 22:39:25 +0530

    Add files via upload

---
+++ program 7.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	int a=5;
+	
+	cout<<"pre-increment= "<<(++a)<<endl;
+	
+	a=5;
+	 cout<<"post-increment ="<<(a++)<<endl;
+	 
+	 int b=5;
+	 
+	 cout<<"pre-increment="<<(--b)<<endl;
+	 b=5;
+	 
+	 cout<<"post-increment="<<(b--)<<endl;
+	 
+	 return 0;
+}
