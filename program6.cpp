368c3c185cc083b087a5f3326b5ca28c75f80341
shobana0704 <shobzz0704@gmail.com>
2026-07-09 22:19:44 +0530

    Add files via upload

---
+++ program6.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int n=20;
+	
+	cout<<"n+=5: "<<(n+=5)<<endl;
+	cout<<"n-=3: "<<(n-=3)<<endl;
+	cout<<"n*=2: "<<(n*=2)<<endl;
+	cout<<"n/=4: "<<(n/=4)<<endl;
+	cout<<"n%=3: "<<(n%=3)<<endl;
+	
+	return 0;
+} 
