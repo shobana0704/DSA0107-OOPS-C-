9346bd1b5cab3192d1715f3e99dd90f0a64ca97f
shobana0704 <shobzz0704@gmail.com>
2026-07-09 21:35:36 +0530

    Add files via upload

--- program 3.cpp
+++ program 3.cpp
@@ -1,22 +1,19 @@
-#include<iostream>
-
-using namespace  std;
+#include <iostream>
+using namespace std;
 
 int main()
 {
-	int a,b;
-	
-	cout<< "enter two numbers:";
-	cin>>a>>b;
-	
-	cout<<"addition = "<<a+b<<endl;
-	cout<<"subtraction ="<<a-b<<endl;
-	cout<<"multiplication = "<<a*b<<endl;
-	if(b!=0)
-	 cout<<"division = "<<a/b<<endl;
-	 else
-	 cout<<"divison is not possible";
-	 
-	 return 0;
+    int a, b;
 
+    cout << "Enter two numbers: ";
+    cin >> a >> b;
+
+    cout << "a == b : " << (a == b) << endl;
+    cout << "a != b : " << (a != b) << endl;
+    cout << "a > b  : " << (a > b) << endl;
+    cout << "a < b  : " << (a < b) << endl;
+    cout << "a >= b : " << (a >= b) << endl;
+    cout << "a <= b : " << (a <= b) << endl;
+
+    return 0;
 }
