ed0fcf1b86e50829afee5ce0852ba7bc6c13e8cb
shobana0704 <shobzz0704@gmail.com>
2026-07-09 19:51:28 +0530

    Add files via upload

---
+++ program 1.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    
+    int a=10;
+    float b=3.14f;
+    double c=9.99;
+    char d='A';
+    bool e=true;
+
+    
+    
+    cout <<"Int a = " << a <<endl;
+    cout <<"Float b = " << b <<endl;
+    cout <<"Double c = " << c <<endl;
+    cout <<"Char d = " << d <<endl;
+    cout <<"Bool e = " << e <<endl;
+ return 0;
+}
