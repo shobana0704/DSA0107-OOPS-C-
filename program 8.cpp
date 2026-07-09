a1360b6d8641d41b969c0825d6974e3057ad7119
shobana0704 <shobzz0704@gmail.com>
2026-07-09 23:01:49 +0530

    Add files via upload

---
+++ program 8.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int a=5;
+	float b=a;
+	
+	cout<<"float="<<b<<endl;
+	
+	int x=10;
+	float y=2.5;
+	float sum=x+y;
+	
+	cout<<"sum= "<<sum<<endl;
+	
+	float num=12.5;
+	int value=(int)num;
+	
+	cout<<"int value= "<<value<<endl;
+	
+	int n=2,m=8;
+	float result=(float)n/m;
+	
+	cout<<"float result="<<result<<endl;
+	
+	return 0;
+	
+	
+}
